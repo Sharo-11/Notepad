69ae478e71a2f096eb9481d90011fa62593f01c8
Nirakar Jena <jenashubham60@gmail.com>
2022-12-30 13:51:37 +0530

    Background, font, Quick access menu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import font
-from tkinter.messagebox import showinfo
+from tkinter import messagebox
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 import os
 
@@ -47,7 +47,11 @@ def saveFile():
             f.close()
 
 def quitApp():
-    root.destroy()
+    ask=messagebox.askyesno("Exit","Are you sure?",icon="warning")
+    if ask==1:
+        root.destroy()
+    else:
+        return
 
 def cut():
     TextArea.event_generate(("<<Cut>>"))
@@ -61,16 +65,43 @@ def paste():
 def about():
     showinfo("Hackerzz Notepad", "Notepad by Zacker")
 
+def popup_menu(event):
+    quick.tk_popup(event.x_root,event.y_root)
+
+def black():
+    TextArea.config(bg="Black",foreground="green")
+    root.update
+
+def white():
+    TextArea.config(bg="white",foreground="black",insertbackground="black")
+    root.update
+
+def dark():
+    TextArea.config(bg="#272727",foreground="#ffffff",insertbackground="#ffffff")
+    root.update
+
+def lucida():
+    TextArea.config(font="Lucida 13")
+    root.update
+
+def garamond():
+    TextArea.config(font="Garamond")
+    root.update
+
+def courier():
+    TextArea.config(font="Courier 13")
+    root.update
+
 if __name__ == '__main__':
     #Basic Setup
     root = Tk()
     root.title("Untitled - Notepad")
     root.wm_iconbitmap("images/HACKERZZ.ico")
-    root.geometry("644x780+0+0")
-    root.config(bg = "black")
-    
+    root.geometry("644x780")
+    root.config(bg = "white")
+
     #TextArea
-    TextArea = Text(root, font = "Courier 13", background = "black", foreground = "#0af20a", cursor = "xterm green", insertbackground = "#0af20a")
+    TextArea = Text(root, font = "Courier 13", background = 'black', foreground = "light green", insertbackground = "#0af20a")
     file = None
     TextArea.pack(expand = True, fill = BOTH)
    
@@ -109,7 +140,33 @@ if __name__ == '__main__':
     MenuBar.add_cascade(label = "Help", menu = HelpMenu)
     
     root.config(menu = MenuBar)
+
+    #Settings menu
+    Setting=Menu(MenuBar,tearoff=0)
+    background=Menu(Setting,tearoff=0)
+    f_ont=Menu(Setting,tearoff=0)
+
+    background.add_radiobutton(label="Default",command=black)
+    background.add_radiobutton(label="Dark",command=dark)
+    background.add_radiobutton(label="light",command=white)
+
+    f_ont.add_radiobutton(label="lucida",command=lucida)
+    f_ont.add_radiobutton(label="Courier 13",command=courier)
+    f_ont.add_radiobutton(label="Garamond",command=garamond)
+
+    Setting.add_cascade(label="Background",menu=background)
+    Setting.add_cascade(label="font",menu=f_ont)
+    MenuBar.add_cascade(label="Setting",menu=Setting)
     
+    # quick access menu (right click menu)
+    quick=Menu(MenuBar,tearoff=0)
+    quick.add_command(label="Cut",command=cut)
+    quick.add_command(label="Copy",command=copy)
+    quick.add_command(label="Paste",command=paste)
+    quick.add_separator
+    quick.add_command(label="Exit",command=quitApp)
+    
+    root.bind("<Button-3>",popup_menu)
     #SCROLLBAR
     Scroll = Scrollbar(TextArea)
     Scroll.pack(side = RIGHT, fill = Y)
